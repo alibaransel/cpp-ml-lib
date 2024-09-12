394aff7f38b6911ecd9569a3c944ca1c54cbe4ad
Ali Baransel ORAL <57151239+alibaransel@users.noreply.github.com>
2024-09-12 21:42:30 +0300

    Create classes for neural network

--- neural-network.cpp
+++ neural-network.cpp
@@ -1,3 +1,22 @@
-int main()
-{
-}
+class Network {
+   private:
+    /* data */
+   public:
+    Network(/* args */) {}
+};
+
+class Layer {
+   private:
+    /* data */
+   public:
+    Layer(/* args */) {}
+};
+
+class Neuron {
+   private:
+    /* data */
+   public:
+    Neuron(/* args */) {}
+};
+
+int main() {}
